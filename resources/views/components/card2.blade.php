e27b3b36dc10a2d2645c02695b276eee17d8b902
Abdul Hadi <Abdulhadijatoi@gmail.com>
2025-02-01 09:01:54 +0400

    updated contact us page

--- resources/views/components/card2.blade.php
+++ resources/views/components/card2.blade.php
@@ -1,8 +1,17 @@
-<div class="w-full mx-auto max-w-xs overflow-hidden bg-white">
+{{-- <div class="w-full mx-auto max-w-xs overflow-hidden bg-white">
     <img class="object-cover w-full h-56" src="{{ url($card_image) }}" alt="card_image">
 
     <div class="py-5 text-center">
         <a href="#" class="block text-xl font-bold text-gray-800" tabindex="0" role="link">{!! $card_title !!}</a>
-        <span class="text-sm text-gray-700">{!! $card_text !!}</span>
+        <span class="text-sm text-gray-700"></span>
+    </div>
+</div> --}}
+
+ <div class="relative overflow-hidden select-none p-2 text-gray-700">
+    <div class="flex flex-col justify-between rounded-md p-6">
+        <div class="space-y-2">
+            <p class="text-lg font-semibold">{!! $card_title !!}</p>
+            <p class="text-sm text-muted-foreground">{!! $card_text !!}</p>
+        </div>
     </div>
 </div>
--- resources/views/pages/contact-us.blade.php
+++ resources/views/pages/contact-us.blade.php
@@ -4,12 +4,15 @@
 
 @section('content')
     
-    <div class="mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl py-10 px-5 page-content">
-        <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 my-10">
+    <div class="bg-[#f8f9fa]">
+    <div class="mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl page-content py-10 text-center">
+        {{-- <h2 class="text-3xl font-semibold mb-4">Where you can find us</h2> --}}
+        {{-- <p class="text-gray-600 mb-6">Receive news and insights that help you navigate supply chains, understand industry trends, and shape your logistics strategy.</p> --}}
+        <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
             @include('components.card2', [
                 'card_image' => 'storage/uploads/contact_us_image1.png',
                 'card_title' => "United Kingdom, Europe Region", 
-                'card_text' => '<strong>8A, Water Lane Stratford</strong><br>
+                'card_text' => '8A, Water Lane Stratford<br>
                                 E15 4NH, London, GB<br>
                                 <a href="tel:+447979817239">+44-7979-817239</a><br>
                                 gb@lexusline.co.uk',
@@ -18,7 +21,7 @@
             @include('components.card2', [
                 'card_image' => 'storage/uploads/contact_us_image1.png',
                 'card_title' => "Pakistan, Asia Region", 
-                'card_text' => '<strong>305, Ibrahim Trade Tower Shahrah-E-Faisal</strong><br>
+                'card_text' => '305, Ibrahim Trade Tower Shahrah-E-Faisal<br>
                                     75500, Karachi, PK<br>
                                     <a href="tel:+92213458118">++92-21-3458118</a><br>
                                     pk@lexusline.co.uk',
@@ -26,26 +29,37 @@
             @include('components.card2', [
                 'card_image' => 'storage/uploads/contact_us_image1.png',
                 'card_title' => "UAE, MENA Region", 
-                'card_text' => '<strong>901, API World Tower Sheikh Zayed Road</strong><br>
+                'card_text' => '901, API World Tower Sheikh Zayed Road<br>
                                 232268, Dubai AE<br>
                                 <a href="tel:+97143204766">+971-4-3204766</a><br>
                                 ae@lexusline.co.uk',
             ])
         </div>
+    </div>
+    </div>
 
-        <div class="container mx-auto p-6 flex flex-col lg:flex-row items-center gap-8">
+    <div class="mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl page-content">
+    
+        <div class="container mx-auto flex flex-col lg:flex-row items-center gap-8">
             <!-- Graphic Section -->
             <div class="w-full lg:w-1/2 flex justify-center">
-                <img src="{{ 'storage/uploads/contact_us_image.webp' }}" alt="Person on couch with laptop" class="w-full max-w-sm">
+                <img src="{{ asset('assets/images/contact-us.jpg') }}" alt="Person on couch with laptop" class="">
             </div>
 
             <!-- Newsletter Form Section -->
             <div class="w-full lg:w-1/2">
-                <h2 class="text-3xl font-semibold mb-4">Sign up for our logistics newsletters</h2>
+                <h2 class="text-3xl font-semibold mb-4">If you need any help you can contact us</h2>
                 <p class="text-gray-600 mb-6">Receive news and insights that help you navigate supply chains, understand industry trends, and shape your logistics strategy.</p>
                 
                 <form class="space-y-4" method="POST" action="{{ route('subscribe') }}">
                     @csrf
+                    <div>
+                        <label for="name" class="block text-sm font-medium text-gray-700">Name</label>
+                        <div class="mt-1 relative rounded-md shadow-sm">
+                            <input type="text" id="name" name="name" required placeholder="Name" 
+                                class="w-full px-4 py-3 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500">
+                        </div>
+                    </div>
                     <div>
                         <label for="email" class="block text-sm font-medium text-gray-700">Email address</label>
                         <div class="mt-1 relative rounded-md shadow-sm">
@@ -53,7 +67,14 @@
                                 class="w-full px-4 py-3 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500">
                         </div>
                     </div>
-                    <button type="submit" class="w-full bg-blue-900 text-white font-medium py-3 rounded-md hover:bg-blue-800">Submit</button>
+                    <div>
+                        <label for="message" class="block text-sm font-medium text-gray-700">Message</label>
+                        <div class="mt-1 relative rounded-md shadow-sm">
+                            <textarea id="message" name="message" required placeholder="Write message..." class="w-full px-4 py-3 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500">
+                            </textarea>
+                        </div>
+                    </div>
+                    <button type="submit" class="w-full bg-blue-900 text-white font-medium py-3 rounded-md hover:bg-blue-800">Submit Now</button>
                 </form>
 
                 <p class="text-xs text-gray-500 mt-4">
@@ -64,9 +85,9 @@
                 </p>
             </div>
         </div>
-
-
     </div>
+
+    
     
 @endsection
 
