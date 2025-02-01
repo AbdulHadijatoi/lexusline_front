00a25d28634d49e5a39f32d2bf7c5e98db61914d
Abdul Hadi <Abdulhadijatoi@gmail.com>
2025-02-01 09:13:39 +0400

    updated contact us page

--- resources/views/pages/contact-us.blade.php
+++ resources/views/pages/contact-us.blade.php
@@ -3,51 +3,16 @@
 
 
 @section('content')
-    
-    <div class="bg-[#f8f9fa]">
-    <div class="mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl page-content py-10 text-center">
-        {{-- <h2 class="text-3xl font-semibold mb-4">Where you can find us</h2> --}}
-        {{-- <p class="text-gray-600 mb-6">Receive news and insights that help you navigate supply chains, understand industry trends, and shape your logistics strategy.</p> --}}
-        <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-            @include('components.card2', [
-                'card_image' => 'storage/uploads/contact_us_image1.png',
-                'card_title' => "United Kingdom, Europe Region", 
-                'card_text' => '8A, Water Lane Stratford<br>
-                                E15 4NH, London, GB<br>
-                                <a href="tel:+447979817239">+44-7979-817239</a><br>
-                                gb@lexusline.co.uk',
-                
-            ])
-            @include('components.card2', [
-                'card_image' => 'storage/uploads/contact_us_image1.png',
-                'card_title' => "Pakistan, Asia Region", 
-                'card_text' => '305, Ibrahim Trade Tower Shahrah-E-Faisal<br>
-                                    75500, Karachi, PK<br>
-                                    <a href="tel:+92213458118">++92-21-3458118</a><br>
-                                    pk@lexusline.co.uk',
-            ])
-            @include('components.card2', [
-                'card_image' => 'storage/uploads/contact_us_image1.png',
-                'card_title' => "UAE, MENA Region", 
-                'card_text' => '901, API World Tower Sheikh Zayed Road<br>
-                                232268, Dubai AE<br>
-                                <a href="tel:+97143204766">+971-4-3204766</a><br>
-                                ae@lexusline.co.uk',
-            ])
-        </div>
-    </div>
-    </div>
-
     <div class="mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl page-content">
     
-        <div class="container mx-auto flex flex-col lg:flex-row items-center gap-8">
+        <div class="container mx-auto flex flex-col lg:flex-row items-center py-4 gap-8">
             <!-- Graphic Section -->
             <div class="w-full lg:w-1/2 flex justify-center">
                 <img src="{{ asset('assets/images/contact-us.jpg') }}" alt="Person on couch with laptop" class="">
             </div>
 
             <!-- Newsletter Form Section -->
-            <div class="w-full lg:w-1/2">
+            <div class="w-full lg:w-1/2 mx-auto px-6">
                 <h2 class="text-3xl font-semibold mb-4">If you need any help you can contact us</h2>
                 <p class="text-gray-600 mb-6">Receive news and insights that help you navigate supply chains, understand industry trends, and shape your logistics strategy.</p>
                 
@@ -86,6 +51,41 @@
             </div>
         </div>
     </div>
+    <div class="bg-[#f8f9fa]">
+    <div class="mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl page-content py-10 text-center">
+        {{-- <h2 class="text-3xl font-semibold mb-4">Where you can find us</h2> --}}
+        {{-- <p class="text-gray-600 mb-6">Receive news and insights that help you navigate supply chains, understand industry trends, and shape your logistics strategy.</p> --}}
+        <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+            @include('components.card2', [
+                'card_image' => 'storage/uploads/contact_us_image1.png',
+                'card_title' => "United Kingdom, Europe Region", 
+                'card_text' => '8A, Water Lane Stratford<br>
+                                E15 4NH, London, GB<br>
+                                <a href="tel:+447979817239">+44-7979-817239</a><br>
+                                gb@lexusline.co.uk',
+                
+            ])
+            @include('components.card2', [
+                'card_image' => 'storage/uploads/contact_us_image1.png',
+                'card_title' => "Pakistan, Asia Region", 
+                'card_text' => '305, Ibrahim Trade Tower Shahrah-E-Faisal<br>
+                                    75500, Karachi, PK<br>
+                                    <a href="tel:+92213458118">++92-21-3458118</a><br>
+                                    pk@lexusline.co.uk',
+            ])
+            @include('components.card2', [
+                'card_image' => 'storage/uploads/contact_us_image1.png',
+                'card_title' => "UAE, MENA Region", 
+                'card_text' => '901, API World Tower Sheikh Zayed Road<br>
+                                232268, Dubai AE<br>
+                                <a href="tel:+97143204766">+971-4-3204766</a><br>
+                                ae@lexusline.co.uk',
+            ])
+        </div>
+    </div>
+    </div>
+
+    
 
     
     
